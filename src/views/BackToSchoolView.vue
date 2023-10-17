5b887941865e2ac8f2961186fba3e32600fe7150
MartinWidarmadi <2072010@maranatha.ac.id>
2023-10-17 10:27:05 +0700

    little fix on main navbar responsive and fix on header responsive for backToSchool page

--- src/views/BackToSchoolView.vue
+++ src/views/BackToSchoolView.vue
@@ -1,45 +1,7 @@
-<script setup lang="ts"></script>
-
 <template>
   <div>
     <!-- header -->
-    <header>
-      <!-- <div class="bg-gray-200">
-         exit header navbar container 
-        <div class="max-w-5xl py-2 mx-auto">
-           nav exit page 
-          <nav class="flex gap-2 text-xs divide-x-2 divide-gray-400 border-spacing-1">
-            <a href="#">Education Store Home</a>
-            <a href="#" class="ps-2">Exit</a>
-          </nav>
-        </div>
-      </div> -->
-
-      <div class="bg-white">
-        <!-- header container -->
-        <div class="max-w-5xl pt-2 mx-auto">
-          <!-- navbar
-          <MainNavbar /> -->
-
-          <!-- header content -->
-          <div class="max-w-3xl mx-auto">
-            <div class="pt-20 space-y-5 text-center">
-              <h1 class="font-bold text-7xl">Bring on college.</h1>
-              <h2 class="text-2xl">
-                Save on Mac or iPad with education pricing. Plus get a gift card up to
-                $150,footnote1 20% off AppleCare+,and more.
-              </h2>
-              <div>
-                <img
-                  src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/bts-header-202306?wid=1688&hei=528&fmt=png-alpha&.v=1685682466790"
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
+    <BackToSchoolHeader />
 
     <!-- main -->
     <main>
@@ -583,3 +545,7 @@
     </footer>
   </div>
 </template>
+
+<script setup lang="ts">
+import BackToSchoolHeader from '@/components/BackToSchoolComponent/BackToSchoolHeader.vue'
+</script>
