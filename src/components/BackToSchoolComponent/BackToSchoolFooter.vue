9a8cbddd48e9308d639f1f61c27ec0b780245d90
MartinWidarmadi <2072010@maranatha.ac.id>
2023-10-17 13:51:51 +0700

    making footer of backToSchool page to be a component

---
+++ src/components/BackToSchoolComponent/BackToSchoolFooter.vue
@@ -0,0 +1,354 @@
+<template>
+  <footer>
+    <div class="bg-gray-200">
+      <div class="max-w-5xl mx-auto space-y-2 bg-gray-200">
+        <!-- table -->
+        <div class="py-5 border-b-2 border-gray-300">
+          <table class="text-xs border-separate table-auto border-spacing-1">
+            <tr>
+              <td colspan="2">
+                A qualifying Microsoft 365 subscription is required for Word, Excel, PowerPoint, and
+                Outlook.
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">Apple Pencil sold separately.</td>
+            </tr>
+            <tr>
+              <!-- row 1 -->
+              <td class="align-top">1.</td>
+              <td>
+                Qualified Purchasers receive an Apple Gift Card when they purchase an eligible Mac
+                or iPad at a Qualifying Location through 10/2/23. Only one Apple Gift Card per
+                eligible Mac or iPad per Qualified Purchaser. Offer subject to availability. While
+                supplies last. Qualified Purchasers shall receive a discount equal to the value of
+                the Apple Gift Card off the price of the eligible Mac or iPad, but will be charged
+                for all items in their cart, including the Apple Gift Card. Important notice
+                regarding the checkout receipt and monthly statement for Apple Card Monthly
+                Installments (ACMI) purchases with this promotion: Qualified Purchasers selecting
+                ACMI (a 0% APR payment option available only in the U.S.) as payment type at
+                checkout shall receive a discount equal to the value of the Apple Gift Card off the
+                price of the eligible Mac or iPad. This will result in one ACMI installment plan
+                over 12 months for the eligible iPad or Mac discounted by the instant credit, and a
+                second ACMI installment plan over 12 months for the full price of the Apple Gift
+                Card. The total combined amount charged over the two separate ACMI installment plans
+                will reflect the original full retail price of the Eligible Product. Separately,
+                Qualified Purchasers will receive and be charged for the Apple Gift Card in the
+                amount of the applicable discount off the eligible Mac or iPad. ACMI is subject to
+                credit approval and credit limit. Variable APRs for Apple Card other than ACMI range
+                from 19.24% to 29.49% based on creditworthiness. Rates as of August 10, 2023. If you
+                choose the pay-in-full or one-time-payment option for an ACMI-eligible purchase
+                instead of choosing ACMI as the payment option at checkout, that purchase will be
+                subject to the variable APR assigned to your Apple Card. Taxes and shipping are not
+                included in ACMI transactions and are subject to your standard purchase APR. See the
+                Apple Card Customer Agreement at
+                https://www.goldmansachs.com/terms-and-conditions/Apple-Card-Customer-Agreement.pdf(Opens
+                in a new window) for more information. ACMI is not available for purchases made
+                online at the following special stores: Apple Employee Purchase Plan; participating
+                corporate Employee Purchase Programs; Apple at Work for small businesses;
+                Government, and Veterans and Military Purchase Programs, or on refurbished devices.
+                Apple Card is issued by Goldman Sachs Bank USA, Salt Lake City Branch. Available for
+                qualifying applicants in the United States. If you reside in the U.S. territories,
+                please call Goldman Sachs at 877-255-5923 with questions about accessing this offer
+                or applying for Apple Card. This offer cannot be combined with the Apple Employee
+                Purchase Plan or business loyalty pricing. Additional restrictions apply. View full
+                terms and conditions of offer here(Opens in a new window).
+              </td>
+            </tr>
+            <!-- row 2 -->
+            <tr>
+              <td rowspan="3" class="align-top">2.</td>
+              <td>
+                This promotional Apple Education Pricing on AppleCare+ is available for eligible
+                products purchased during the promotional period only and cannot be combined with
+                non-promotional Apple Education Pricing. AppleCare+ attached to eligible products
+                outside of the promotional period is not eligible for this promotional rate.
+              </td>
+            </tr>
+            <tr>
+              <td>
+                This promotional AppleCare+ Education Pricing is applicable only to two-year paid
+                upfront AppleCare+ for iPad and three-year paid upfront AppleCare+ for Mac and is
+                not applicable to any recurring payment AppleCare+ plans.
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Apple Education Pricing is available to current and newly accepted university
+                students and their parents, as well as teachers and staff at all levels. Quantity
+                limits apply.
+              </td>
+            </tr>
+            <!-- row 3 -->
+            <tr>
+              <td class="align-top">3.</td>
+              <td>
+                Monthly pricing is available when you select Apple Card Monthly Installments (ACMI)
+                as payment type at checkout at Apple, and is subject to credit approval and credit
+                limit. Financing terms vary by product. Taxes and shipping are not included in ACMI
+                and are subject to your card’s variable APR. See the Apple Card Customer
+                Agreement(Opens in a new window) for more information. ACMI is not available for
+                purchases made online at special storefronts. The last month’s payment for each
+                product will be the product's purchase price, less all other payments at the monthly
+                payment amount. ACMI financing is subject to change at any time for any reason,
+                including but not limited to, installment term lengths and eligible products. See
+                https://support.apple.com/kb/HT211204(Opens in a new window) for information about
+                upcoming changes to ACMI financing.
+              </td>
+            </tr>
+            <!-- row 4 -->
+            <tr>
+              <td class="align-top">4.</td>
+              <td>
+                Trade-in values will vary based on the condition, year, and configuration of your
+                eligible trade-in device. Not all devices are eligible for credit. You must be at
+                least 18 years old to be eligible to trade in for credit or for an Apple Gift Card.
+                Trade-in value may be applied toward qualifying new device purchase, or added to an
+                Apple Gift Card. Actual value awarded is based on receipt of a qualifying device
+                matching the description provided when estimate was made. Sales tax may be assessed
+                on full value of a new device purchase. In-store trade-in requires presentation of a
+                valid photo ID (local law may require saving this information). Offer may not be
+                available in all stores, and may vary between in-store and online trade-in. Some
+                stores may have additional requirements. Apple or its trade-in partners reserve the
+                right to refuse or limit quantity of any trade-in transaction for any reason. More
+                details are available from Apple’s trade-in partner for trade-in and recycling of
+                eligible devices. Restrictions and limitations may apply.
+              </td>
+            </tr>
+            <!-- row 5 -->
+            <tr>
+              <td rowspan="3" class="align-top">5.</td>
+              <td>
+                Apple Card Monthly Installments (ACMI) is a 0% APR payment option that is only
+                available if you select it at checkout in the U.S. for eligible Apple products
+                purchased at Apple Store locations, apple.com(Opens in a new window), the Apple
+                Store app, or by calling 1-800-MY-APPLE, and is subject to credit approval and
+                credit limit. See https://support.apple.com/kb/HT211204(Opens in a new window) for
+                more information about eligible products. APR ranges may vary based on when you
+                accepted an Apple Card. Cardholders who accept an Apple Card on and/or after August
+                10, 2023: variable APRs for Apple Card, other than ACMI, range from 19.24% to 29.49%
+                based on creditworthiness. Rates as of August 10, 2023. Existing cardholders: see
+                your Customer Agreement for applicable rates and fee. If you buy an ACMI eligible
+                product by choosing to pay in full with Apple Card (instead of using ACMI), that
+                purchase is subject to the Apple Card variable APR, not 0% APR. Taxes and shipping
+                on ACMI purchases are subject to the variable APR, not 0% APR. You’ll need to select
+                AT&T, T-Mobile, or Verizon as your carrier when you check out. An iPhone purchased
+                with ACMI is always unlocked, so you can switch carriers at any time. ACMI is not
+                available for purchases made online at the following special stores: Apple Employee
+                Purchase Plan; participating corporate Employee Purchase Programs; Apple at Work for
+                small businesses; Government, and Veterans and Military Purchase Programs, or on
+                refurbished devices. The last month’s payment for each product will be the product's
+                purchase price, less all other payments at the monthly payment amount. ACMI
+                financing is subject to change at any time for any reason, including but not limited
+                to, installment term lengths and eligible products. See
+                https://support.apple.com/kb/HT211204(Opens in a new window) for information about
+                upcoming changes to ACMI financing. See the Apple Card Customer Agreement(Opens in a
+                new window) for more information about ACMI financing.
+              </td>
+            </tr>
+            <tr>
+              <td>
+                To access and use all Apple Card features and products available only to Apple Card
+                users, you must add Apple Card to Wallet on an iPhone or iPad that supports and has
+                the latest version of iOS or iPadOS. Apple Card is subject to credit approval,
+                available only for qualifying applicants in the United States, and issued by Goldman
+                Sachs Bank USA, Salt Lake City Branch.
+              </td>
+            </tr>
+            <tr>
+              <td>
+                If you reside in the U.S. territories, please call Goldman Sachs at 877-255-5923
+                with questions about Apple Card.
+              </td>
+            </tr>
+            <!-- row 6 -->
+            <tr>
+              <td class="align-top">6.</td>
+              <td>
+                Offer available to new and returning subscribers to the Apple Music Student plan for
+                a limited time. Following a qualifying subscription, offer recipients will receive a
+                notification to redeem 3 months free. Offer must be redeemed via the notification
+                before November 3, 2023. Plan automatically renews at $5.99/month until cancelled.
+                College students only. Verification required. Terms apply.
+              </td>
+            </tr>
+            <!-- row 7 -->
+            <tr>
+              <td class="align-top">7.</td>
+              <td>
+                Limited-time offer; offer may end at any time. Free Apple TV+ access for students
+                ends when you no longer qualify or do not renew your Apple Music Student
+                subscription. Offer good for verified college students only and does not extend to a
+                Family Sharing group.
+              </td>
+            </tr>
+          </table>
+        </div>
+
+        <!-- link back to school -->
+        <div>
+          <div class="flex items-center gap-2">
+            <div>></div>
+            <p class="text-xs">Back to School - Education Discounts</p>
+          </div>
+        </div>
+
+        <!-- all links -->
+        <div>
+          <div class="grid grid-cols-5 my-5 text-xs">
+            <!-- first col -->
+            <div class="grid grid-cols-1 gap-1 grid-row-2">
+              <!-- shop and learn links -->
+              <div class="flex flex-col gap-2">
+                <h5 class="font-semibold">Shop and Learn</h5>
+                <a href="#">Store</a>
+                <a href="#">Mac</a>
+                <a href="#">iPad</a>
+                <a href="#">iPhone</a>
+                <a href="#">Watch</a>
+                <a href="#">Vision</a>
+                <a href="#">AirPods</a>
+                <a href="#">TV & Home</a>
+                <a href="#">Air Tag</a>
+                <a href="#">Accessories</a>
+                <a href="#">Gift Cards</a>
+              </div>
+              <!-- apple wallet links -->
+              <div class="flex flex-col gap-2">
+                <h5 class="font-semibold">Apple Wallet</h5>
+                <a href="#">Wallet</a>
+                <a href="#">Apple Card</a>
+                <a href="#">Apple Pay</a>
+                <a href="#">Apple Cash</a>
+              </div>
+            </div>
+
+            <!-- second col -->
+            <div class="grid grid-cols-1 gap-1 grid-row-2">
+              <!-- Account links -->
+              <div class="flex flex-col gap-2">
+                <h5 class="font-semibold">Account</h5>
+                <a href="#">Manage Your Apple ID</a>
+                <a href="#">Apple Store Account</a>
+                <a href="#">iCloud.com</a>
+              </div>
+              <!-- Entertaiment links -->
+              <div class="flex flex-col gap-2">
+                <h5 class="font-semibold">Entertaiment</h5>
+                <a href="#">Apple One</a>
+                <a href="#">Apple TV+</a>
+                <a href="#">Apple Music</a>
+                <a href="#">Apple Arcade</a>
+                <a href="#">Apple Fitness+</a>
+                <a href="#">Apple News+</a>
+                <a href="#">Apple Podcasts</a>
+                <a href="#">Apple Books</a>
+                <a href="#">Apple Store</a>
+              </div>
+            </div>
+
+            <!-- third col -->
+            <div class="grid grid-cols-1 gap-1 grid-row-1">
+              <!-- apple store links -->
+              <div class="flex flex-col gap-2">
+                <h5 class="font-semibold">Apple Store</h5>
+                <a href="#">Find a Store</a>
+                <a href="#">Genius Bar</a>
+                <a href="#">Today at Apple</a>
+                <a href="#">Apple Camp</a>
+                <a href="#">Apple Store App</a>
+                <a href="#">Certified Refurbished</a>
+                <a href="#">Apple Trade In</a>
+                <a href="#">Order Status</a>
+                <a href="#">Shopping Help</a>
+              </div>
+            </div>
+            <!-- fourth col -->
+            <div class="grid grid-cols-1 gap-1 grid-row-4">
+              <!-- for business links -->
+              <div class="flex flex-col gap-2">
+                <h5 class="font-semibold">For Business</h5>
+                <a href="#">Apple and Business</a>
+                <a href="#">Shop for Business</a>
+              </div>
+
+              <!-- for education links -->
+              <div class="flex flex-col gap-2">
+                <h5 class="font-semibold">For Education</h5>
+                <a href="#">Apple and Education</a>
+                <a href="#">Shop for K-12</a>
+                <a href="#">Shop for College</a>
+              </div>
+
+              <!-- for healthcare links -->
+              <div class="flex flex-col gap-2">
+                <h5 class="font-semibold">For Healthcare</h5>
+                <a href="#">Apple in Healthcare</a>
+                <a href="#">Health on Apple Watch</a>
+                <a href="#">Health Records on iPhone</a>
+              </div>
+
+              <!-- for goverment links -->
+              <div class="flex flex-col gap-2">
+                <h5 class="font-semibold">For Goverment</h5>
+                <a href="#">Shop for Goverment</a>
+                <a href="#">Shop for Veterans and Military</a>
+              </div>
+            </div>
+
+            <!-- fifth col -->
+            <div class="grid grid-cols-1 gap-1 grid-row-2">
+              <!-- apple values links -->
+              <div class="flex flex-col gap-2">
+                <h5 class="font-semibold">Apple Values</h5>
+                <a href="#">Accessibility</a>
+                <a href="#">Education</a>
+                <a href="#">Environment</a>
+                <a href="#">Inclusion and Diversity</a>
+                <a href="#">Privacy</a>
+                <a href="#">Racial Equity and Justice</a>
+                <a href="#">Supplier Responsibility</a>
+              </div>
+
+              <!-- about apple links -->
+              <div class="flex flex-col gap-2">
+                <h5 class="font-semibold">About Apple</h5>
+                <a href="#">Newsroom</a>
+                <a href="#">Apple Leadership</a>
+                <a href="#">Career Opportunities</a>
+                <a href="#">Investors</a>
+                <a href="#">Ethics & Compliance</a>
+                <a href="#">Events</a>
+                <a href="#">Contact Apple</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- shop or retailer info -->
+        <div class="pb-2 border-b-2 border-gray-300">
+          <div class="text-xs">
+            More ways to shop: <a href="#">Find an Apple Store</a> or
+            <a href="#">other retailer</a> near you. Or call 1-800-MY-APPLE.
+          </div>
+        </div>
+
+        <!-- copyright stuff -->
+        <div class="pb-2">
+          <div class="flex justify-between text-xs">
+            <div>Copyright © 2023 Apple inc. All rights reserved.</div>
+            <div class="flex gap-2 divide-x-2 divide-gray-400">
+              <a href="#">Privacy Policy</a>
+              <a href="#" class="ps-2">Terms of Use</a>
+              <a href="#" class="ps-2">Sales and Refunds</a>
+              <a href="#" class="ps-2">Site Map</a>
+            </div>
+            <p>United States</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
